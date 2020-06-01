84b0d46fb5e63a0b3b091ed2adca02bba0ca35ab
Steven van Dijk <zteven@gmail.com>
2020-06-01 13:08:37 +0200

    Add state

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from enum import Enum
+
+class State(Enum):
+    EnteringIntegers = 1
+    EnteringFloats = 2
+    Calculating = 3
+    Calculated = 4
 
 class MainApp(App):
+    state: State = State.EnteringIntegers
     memoryLabel: None
     resultLabel: None
     memory = 0.0
@@ -29,25 +37,31 @@ class MainApp(App):
         self.resultLabel.text = str(self.result)
 
     def enter(self, num):
+        if (self.state == State.Calculated):
+            self.state = State.EnteringIntegers
+            self.result = 0.0
         self.result = self.result * 10 + num
 
     def op(self, operator):
+        self.state = State.Calculating
         self.memory = self.result
         self.result = 0.0
         self.lastOperator = operator
 
     def equals(self):
-        plus = self.memory + self.result
-        minus = self.memory - self.result
-        times = self.memory * self.result
-        division = self.memory / self.result
-        if self.lastOperator == '+': calculated = plus
-        elif self.lastOperator == '-': calculated = minus
-        elif self.lastOperator == '*': calculated = times
-        elif self.lastOperator == '/': calculated = division
+        if (self.state == State.Calculating):
+            self.state = State.Calculated
+            plus = self.memory + self.result
+            minus = self.memory - self.result
+            times = self.memory * self.result
+            division = self.memory / self.result
+            if self.lastOperator == '+': calculated = plus
+            elif self.lastOperator == '-': calculated = minus
+            elif self.lastOperator == '*': calculated = times
+            elif self.lastOperator == '/': calculated = division
 
-        self.clear()
-        self.result = calculated
+            self.clear()
+            self.result = calculated
 
     def clear(self):
         self.memory = 0.0
@@ -74,7 +88,7 @@ class MainApp(App):
             { '7': btn(7.0), '8': btn(8.0), '9': btn(9.0), '/': spc('/') },
             { '4': btn(4.0), '5': btn(5.0), '6': btn(6.0), '*': spc('*') },
             { '1': btn(1.0), '2': btn(2.0), '3': btn(3.0), '-': spc('-') },
-            { '.': None, '0': btn(0.0), 'C': lambda: self.clear(), '+': spc('+') },
+            { '.': lambda: self.puntje(), '0': btn(0.0), 'C': lambda: self.clear(), '+': spc('+') },
             { '=': lambda: self.equals() }
         ]
         self.addButtons(totalLayout, buttons)
